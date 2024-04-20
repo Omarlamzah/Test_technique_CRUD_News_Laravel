dd9a556c8595064d07d8e7dbaea19142f2306fac
omar lamzah <omarlamzah64@gmail.com>
2024-04-20 11:55:13 +0000

    initial project CRUD de News en Laravel we say bismilah

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD de News en Laravel</title>
+</head>
+<body>
+
+<div style="text-align: center;display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh">
+
+    <h1>welcome I am omar lamzah </h1>
+    <h1>&& </h1>
+    <h1> this Test technique : CRUD de News en Laravel</h1>
+    <h1> 💻 this project work as rest api not fullstack 💻 </h1>
+
+</div>
+
+</body>
+</html>
